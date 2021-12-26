1639105c085fa783e4a9e21eb8527703e638547d
Jesse Sandvik <sandvikjesse@gmail.com>
2021-12-26 13:16:26 -0500

    selection sort

--- algoProblems/selectionSort.js
+++ algoProblems/selectionSort.js
@@ -8,10 +8,22 @@ const data = require("./data");
  * 
  * **Solve the problem before writing the code. Write your pseudocode here.**
  * 
- * We're going to make two sublists in this array,
- * one will be the unsorted sublist,
- * one will be the sorted sublist.
+ *  This problem is solved using the selection sort algorithm.
  * 
- * As the sorted sublist grows, the unsorted sublist will shrink,
- * until the entire list becomes one sorted list.
+ *  This problem can be solved by dividing the list into two sublists,
+ *  one list will contain the unsorted elements, one list will contain the sorted elements.
+ * 
+ *  The sorted sublist will be empty to start, and will fill as the unsorted list shrinks,
+ *  ultimately resulting in all elements being sorted in the sorted sublist, and the unsorted sublist being empty.
+ * 
+ *  1) I will initialize a variable and assign it the first index of the unsorted sublist.
+ *  Since the entire list begins unsorted, this will be the first index of the input array/list.
+ * 
+ *  2) If I want to sort the array/list in ascending order,
+ *  I will iterate through the input list/array to find the index of the smallest element and assign it to a variable.
+ * 
+ *  If I want to sort the array/list in descending order,
+ *  I will iterate through the input list/array to find the index of the largest element and assign it to a variable.
+ * 
+ *  1) Initialize a variable and assign it to the first index of the unsorted sublist.
  */
