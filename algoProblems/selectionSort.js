f8de9a6b53078c046c3705eb5eb8a530301d9eef
Jesse Sandvik <sandvikjesse@gmail.com>
2021-12-26 14:05:54 -0500

    selection sort

--- algoProblems/selectionSort.js
+++ algoProblems/selectionSort.js
@@ -21,7 +21,7 @@ const data = require("./data");
  * 
  *  2) I will have to iterate through the input list/array two times;
  * 
- *  If I'm sorting the list list in ascending order,
+ *  If I'm sorting the list in ascending order,
  *  one interation will be to find the index of the smallest element.
  * 
  *  If I'm sorting the list in descending order,
@@ -30,9 +30,54 @@ const data = require("./data");
  *  Regardless, the second iteration will be used to swap the smallest/largest element,
  *  with the first index of the unsorted sublist.
  * 
+ *  After this swap, the unsorted first index will be incremented by one.
+ * 
  *  The second iteration will end when the first index of the unsorted sublist,
  *  is no longer less than the last index of the unsorted sublist.
  *  This conclusion will mean that all elements are now in the sorted sublist.
  * 
  *  3) Return the sorted list/array.
+ * 
+ * function selectionSort
+ *  input: array - an unsorted array/list
+ * 
+ *  output: a sorted array/list
+ * 
+ *  1) Initialize a variable and assign it to the first index of the unsorted sublist.
+ *  2) Iterate through the array/list to find the index of the smallest/largest element in the unsorted sublist.
+ *  3) Iterate through the array/list and swap the smallest/largest element fronm the previous iteration
+ *     with the element at the current index of the unsorted sublist.
+ *     Then increment the first index of the unsorted sublist by 1.
+ *     End the iteration when:
+ *     the first index of the unsorted sublist is no longer less than the last index of the input array/list.
+ *  4) Return the sorted list/array.
+ */
+
+//  Time: O(n^2) | Space: O(1)
+function selectionSort(array) {
+    let unsortedFirstIndex = 0;
+
+    while (unsortedFirstIndex < array.length) {
+        let smallestElementIndex = unsortedFirstIndex;
+
+        for (let i = unsortedFirstIndex + 1; i < array.length; i++) {
+            if (array[i] < array[smallestElementIndex]) {
+                smallestElementIndex = i;
+            }
+        }
+            const temp = array[unsortedFirstIndex];
+            array[unsortedFirstIndex] = array[smallestElementIndex];
+            array[smallestElementIndex] = temp;
+            unsortedFirstIndex++;
+        }
+    return array;
+}
+
+console.log(selectionSort(data.sequenceOne));
+console.log(selectionSort(data.sequenceTwo));
+
+/**
+ * Selection sort works just fine, and isn't terrible in terms of efficency.
+ * However, there are better options for sorting algorithms,
+ * such as quicksort which has a time complexity of O(n log(n)).
  */
