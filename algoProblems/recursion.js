7cea7904e65ad39f668badaa7660c7de7b4650d9
Jesse Sandvik <sandvikjesse@gmail.com>
2021-12-26 16:20:34 -0500

    recursion

---
+++ algoProblems/recursion.js
@@ -0,0 +1,20 @@
+const data = require("./data");
+
+/**
+ * Implement a recursive factorial function.
+ * 
+ * 
+ * 
+ * **Solve the problem before writing the code. Write your pseudocode here.**
+ * 
+ *  This problem is solved as an example of a recursive function.
+ *  Recursion is a problem-solving method that involves a function calling itself.
+ *  Think of recursion as a way to solve a smaller version
+ *  of a potentially much larger problem you're trying to solve.
+ *  
+ * *Note of caution when using recursion*
+ * 
+ *  Because recursion is a problem-solving method that repeatedly calls itself,
+ *  it can be very easy to write a function incorrectly that results in an infinite loop.
+ *  Remember to establish a base case before running your code.
+ */
