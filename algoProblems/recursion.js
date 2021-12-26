d0741b0153b611d6a667e2132b48f7cf781070ff
Jesse Sandvik <sandvikjesse@gmail.com>
2021-12-26 16:34:17 -0500

    recursion

--- algoProblems/recursion.js
+++ algoProblems/recursion.js
@@ -9,12 +9,26 @@ const data = require("./data");
  * 
  *  This problem is solved as an example of a recursive function.
  *  Recursion is a problem-solving method that involves a function calling itself.
+ * 
  *  Think of recursion as a way to solve a smaller version
- *  of a potentially much larger problem you're trying to solve.
+ *  of a potentially much larger problem that you're really trying to solve.
+ * 
+ *  Recursive functions have two parts:
+ * 
+ *  1) The base case,
+ *  which is the smaller(simplest) version of the problem you're trying to solve.
+ * 
+ *  2) The recursive case,
+ *  which is where the function calls itself,
+ *  solving increasingly simpler versions of that same problem.
+ * 
+ *  Recursive algorithms can have more than one base or recursive case,
+ *  depending on inputs, outputs, conditions, etc
  *  
  * *Note of caution when using recursion*
  * 
  *  Because recursion is a problem-solving method that repeatedly calls itself,
  *  it can be very easy to write a function incorrectly that results in an infinite loop.
  *  Remember to establish a base case before running your code.
+ *
  */
